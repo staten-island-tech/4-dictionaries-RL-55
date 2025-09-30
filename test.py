909479f24833a7a7fb2be3ed206a6fd4445fc22f
RL-55 <rickyl55@nycstudents.net>
2025-09-30 09:24:14 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+item = {
+    "name": "Samsung 55\" 4K UHD TV",
+    "price": 429.99,
+    "weight": 35,
+    }
+{
+    "name": "Samsung\" 4K UHD TV",
+    "price": 429.99,
+    "weight": 35,
+    }
+print(item[1]["name"])
