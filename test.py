3ce9185342da4b9d4d1e3d3d2c95c8bd426c059d
RL-55 <rickyl55@nycstudents.net>
2025-10-01 09:33:25 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,27 @@
-item = {
-    "name": "Samsung 55\" 4K UHD TV",
-    "price": 429.99,
-    "weight": 35,
+SnowdinShop={
+    "item1"={
+        "name": "Bisicle",
+        "price":"15G",
+        "effect":"11 HP",
+        "description":"It's a two-pronged popsicle, so you can eat it twice."
     }
-{
-    "name": "Samsung\" 4K UHD TV",
-    "price": 429.99,
-    "weight": 35,
+}
+
+item1 = {
+        "name": "Bisicle",
+        "price":"15G",
+        "effect":"11 HP",
+        "description":"It's a two-pronged popsicle, so you can eat it twice."
     }
-print(item[1]["name"])
+item2={
+        "name":"Cinnamon Bunny",
+        "price":"25G",
+        "effect":"22 HP",
+        "description":"A cinnamon roll in the shape of a bunny."
+    },
+item3={
+    "name":"Manly Bandanna",
+    "price":"50G",
+    "effect":"7 DF",
+    "description":"It has seen some wear. It has abs drawn on it."
+}
