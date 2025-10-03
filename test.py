225b23c8afde8157ba635872d3552caf4623a423
RL-55 <rickyl55@nycstudents.net>
2025-10-03 09:11:34 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,27 +1,27 @@
-SnowdinShop={
-    "item1"={
-        "name": "Bisicle",
-        "price":"15G",
-        "effect":"11 HP",
-        "description":"It's a two-pronged popsicle, so you can eat it twice."
-    }
-}
-
 item1 = {
-        "name": "Bisicle",
-        "price":"15G",
-        "effect":"11 HP",
-        "description":"It's a two-pronged popsicle, so you can eat it twice."
-    }
+    "name": "Bisicle",
+    "price":"15G",
+    "effect":"11HP",
+    "description":"It's a two-pronged popsicle, so you can eat it twice."
+}
 item2={
         "name":"Cinnamon Bunny",
         "price":"25G",
         "effect":"22 HP",
         "description":"A cinnamon roll in the shape of a bunny."
-    },
+        }
 item3={
     "name":"Manly Bandanna",
     "price":"50G",
     "effect":"7 DF",
     "description":"It has seen some wear. It has abs drawn on it."
-}
+    }
+SnowdinShop=[item1,item2,item3]
+print(SnowdinShop[0])
+def shop():
+    x=0
+    print("Hello, traveller. How can I help you?")
+    while not x==1:
+        for i in range(2):
+            print(SnowdinShop)
+        y=input("What would you like to buy? ")
