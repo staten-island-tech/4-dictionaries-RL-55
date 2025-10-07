217c00162545d34bfd7fa81fadc021cc30ca50e4
RL-55 <rickyl55@nycstudents.net>
2025-10-07 09:01:20 -0400

    Update Assesement.py

--- Assesement.py
+++ Assesement.py
@@ -1,12 +1,22 @@
-x=input("Write a sentence. ")
-y=0
-z=0
-for i in x:
-    if i=="t" or i=="T":
-        y=y+1
-    elif i=="s" or i=="S":
-        z=z+1
-if y>=z:
-    print("English")
-else:
-    print("French")
+def englishorfrench():
+    x=input("Write a sentence. ")
+    y=0
+    z=0
+    for i in x:
+        if i=="t" or i=="T":
+            y=y+1
+        elif i=="s" or i=="S":
+            z=z+1
+    if y>=z:
+        print("French")
+    else:
+        print("English")
+def parking():
+    x=input("Write . for empty C for occupied ")
+    y=input("Write . for empty C for occupied ")
+    z=0
+    for i in x:
+        if x==y and i=="C":
+            z=z+1
+    print(z)
+parking()
