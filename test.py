254bf6bd54867481009d289e3eb402488bb4fdbb
RL-55 <rickyl55@nycstudents.net>
2025-10-08 09:26:01 -0400

    Update test.py

--- test.py
+++ test.py
@@ -2,39 +2,59 @@ item1 = {
     "name": "Bisicle",
     "price":"15G",
     "effect":"11HP",
-    "description":"It's a two-pronged popsicle, so you can eat it twice."
+    "desciption":"It's a two-pronged popsicle, so you can eat it twice."
     }
 item2={
     "name":"Cinnamon Bunny",
     "price":"25G",
     "effect":"22 HP",
-    "description":"A cinnamon roll in the shape of a bunny."
+    "desciption":"A cinnamon roll in the shape of a bunny."
     }
 item3={
     "name":"Manly Bandanna",
     "price":"50G",
     "effect":"7 DF",
-    "description":"It has seen some wear. It has abs drawn on it."
+    "desciption":"It has seen some wear. It has abs drawn on it."
     }
-SnowdinShop=(item1,item2,item3)
+SnowdinShop=(item1,item2,item3,"Exit")
 def shop():
     cart=[]
     x=0
     print("Hello, traveller. How can I help you?")
     while not x==1:
-        for i in range(2):
-            print(SnowdinShop[i+1]["name"])
+        for i in range(3):
+            print(SnowdinShop[i]["name"])
         y=input("What would you like to buy? ")
         if y=="Bisicle":
-            print(item1)
-            z=input("Buy? Yes,No ")
+            for h in item1:
+                print(SnowdinShop[0][h])
+            z=input("Are you sure? ")
             if z=="Yes":
                 print("Thanks for your purchase.")
-                cart.append(item1)
-        elif y=="Cinnamon Bunny":
-            print(item2)
+                cart.append(item1["name"])
+            else:
+                print("Just looking?")
+        elif y=="Cinnamon Bunny":    
+                    for h in item1:
+                print(SnowdinShop[0][h])
+            z=input("Are you sure? ")
+            if z=="Yes":
+                print("Thanks for your purchase.")
+                cart.append(item1["name"])
+            else:
+                print("Just looking?")            
         elif y=="Manly Bandanna":
-            print(item3)
-        else:
-            print("That is not in my shop.")
+                        for h in item1:
+                print(SnowdinShop[0][h])
+            z=input("Are you sure? ")
+            if z=="Yes":
+                print("Thanks for your purchase.")
+                cart.append(item1["name"])
+            else:
+                print("Just looking?")
+        elif y=="Exit":
+            print("Bye now! Come again sometime!")
+            print(f"You bought{cart}")
+            x=1
+
 shop()
