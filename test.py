b3c6bd8b7fe5af7b30ab2610d888edeaa625e375
RL-55 <rickyl55@nycstudents.net>
2025-10-17 09:26:17 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 SnowdinShop={
-"item1" : {
+"1" : {
     "name": "Bisicle",
-    "price":f"{15}G",
+    "price":15,
     "effect":"11HP",
     "desciption":"It's a two-pronged popsicle, so you can eat it twice."},
-"item2":{
+"2":{
     "name":"Cinnamon Bunny",
     "price":25,
     "effect":"22 HP",
     "desciption":"A cinnamon roll in the shape of a bunny."},
-"item3":{
+"3":{
     "name":"Manly Bandanna",
     "price":50,
     "effect":"7 DF",
@@ -17,28 +17,26 @@ SnowdinShop={
 }
 def shop():
     cart=[]
-    x=0
-    z=0
+    off=0
+    G=0
     print("Hello, traveller. How can I help you?")
-    while not x==1:
+    while not off==1:
         for a in SnowdinShop:
             print(SnowdinShop[a]["name"])
         y=input("What would you like to buy: ")
         for b in SnowdinShop:
             if y==SnowdinShop[b]["name"]:
-                for c in SnowdinShop["item1"]:
+                for c in SnowdinShop["1"]:
                     print(SnowdinShop[b][c])
                 y=input("Are you sure? Yes or No ")
                 if y=="Yes" or y=="yes":
                     print("Thanks for your purchase")
                     cart.append(SnowdinShop[b]["name"])
-                    z+=SnowdinShop[b]["price"]
+                    G+=SnowdinShop[b]["price"]
                     y=input("Buy or Exit: ")
                     if y=="Buy" or y=="buy":
-                        x=0
+                        off=0
                     elif y=="Exit" or y=="exit":
-                        x=1
-                        print(f"You purchased {cart}",f"Price: {z}G")
-            else:
-                print("")
+                        off=1
+                        print(f"You purchased {cart}",f"Price: {G}G")
 shop()
